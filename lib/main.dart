ba4b6015591e9cbc5b49928f47a83906a91e5fbf
JamelChatti <chatti261@gmail.com>
2020-09-21 08:18:57 +0200

    Raisedbutton

--- lib/main.dart
+++ lib/main.dart
@@ -22,54 +22,55 @@ class GestionFacture extends StatefulWidget {
 }
 
 class _GestionFactureState extends State<GestionFacture> {
-String value = 'Bonjour';
-String value1 = 'c\'est le bouton 2';
-  var info = 'RaisedButton';
-@override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    info= 'BIENVENUE';
-  }
-  void onclick(){
-  setState(() {
-    value = 'Tuto RaisedButton';
-  });
-  }
+  int note = 0;
 
-void onclick2(String value2){
-  setState(() {
-    value1 = value2;
-  });
-}
+@override
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(info),
+        title: Text('Bienvenue'),
       ),
       body:  Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: <Widget> [
-        RaisedButton(
-          child: Text('bouton 1', style: TextStyle(color: Colors.green),
-          ),
-          onPressed:() => onclick(),
-        ),
-        Text(value, style: TextStyle(color: Colors.blue, fontSize: 40,),
-
-        ),
-        Divider(height: 10,),
-
-        RaisedButton(
-          child: Text('bouton 2', style: TextStyle(color: Colors.blue),
-          ),
+        FlatButton(
           onPressed: (){
-            onclick2('Tuto pour les debutants');
+            debugPrint('---Bouton flat ok-----');
+              },
+          child: Text('Bouton flat', style: TextStyle(color: Colors.white, fontSize: 20,),),
+          color: Colors.blue,
+        ),
+
+        IconButton(
+          icon: Icon(Icons.thumb_up, color: Colors.green, size: 60,),
+          tooltip: 'j\'aime',
+          onPressed: (){
+            setState(() {
+
+            });
+            note+=1;
           },
         ),
-         Text(value1, style: TextStyle(color: Colors.green, fontSize: 40,),)
+
+
+        Text('Votre point de vue: $note', style: TextStyle(color: Colors.indigo, fontSize: 20,),
+
+        ),
+
+
+        IconButton(
+          icon: Icon(Icons.thumb_down, color: Colors.red, size: 60,),
+          tooltip: 'je n\'aime pas',
+          onPressed: (){
+            setState(() {
+
+            });
+            note-=1;
+          },
+        ),
+
 
       ],
       ),
