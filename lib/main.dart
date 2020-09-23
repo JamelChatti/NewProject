e8d308f48a309700a876d8c8e0d855a43320f616
JamelChatti <chatti261@gmail.com>
2020-09-23 12:54:41 +0200

    FooterButton & SheetButton

--- lib/main.dart
+++ lib/main.dart
@@ -23,13 +23,46 @@ class GestionFacture extends StatefulWidget {
 }
 
 class _GestionFactureState extends State<GestionFacture> {
+  String value = '';
+
+  void show(String a) {
+    setState(() {
+      value = a;
+    });
+  }
+
+  void shows(String a) => setState(() => value = a);
+
+  void bouton_Sheet (){
+    showModalBottomSheet(context: context, builder: (BuildContext context){
+
+      return Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            Text(
+              'Soyez le bienvenue',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 18,
+
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.cyan,
-          title: Text('Tuto AppBar'),
+          title: Text('Tuto FooterButton & SheetBottun'),
           centerTitle: true,
           actions: <Widget>[
             Icon(
@@ -37,48 +70,64 @@ class _GestionFactureState extends State<GestionFacture> {
               size: 35,
             ),
           ],
-
         ),
-        drawer: ListView(padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text(
-          'Programe & framework',
-          style: TextStyle(fontSize: 25, color: Colors.red
-              ),
+        persistentFooterButtons: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.zoom_out_map,
             ),
-              decoration: BoxDecoration(color: Colors.black),
-            ),
-            ListTile(title: Text('Dart') ,onTap: (){
-              Navigator.pop(context);
-            },),
-
-            ListTile(title: Text('Java') ,onTap: (){
-              Navigator.pop(context);
-            },),
-
-            ListTile(title: Text('Swift') ,onTap: (){
-              Navigator.pop(context);
-            },),
-
-            ListTile(title: Text('Objecti-C') ,onTap: (){
-              Navigator.pop(context);
-            },),
-
-            ListTile(title: Text('Foxpro') ,onTap: (){
-              Navigator.pop(context);
-            },),
-
-            ListTile(title: Text('C++') ,onTap: (){
-              Navigator.pop(context);
-            },),
-          ],
-        ),
+            onPressed: () => show('Zoom map'),
+          ),
+          IconButton(
+            icon: Icon(Icons.account_balance_wallet),
+            onPressed: () => shows('info bank'),
+          ),
+          IconButton(
+            icon: Icon(Icons.add_comment),
+            onPressed: () => show('Credit info'),
+          ),
+        ],
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              
+            ButtonTheme(
+            minWidth: 200.0,
+            height: 75.0,
+            buttonColor: Colors.grey,
+            child: RaisedButton(onPressed: bouton_Sheet , child: Text('Cliquer ici', style: TextStyle(fontSize: 20, ), ),),
+          ),
+
+              Text(
+                'bienvenue',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 18,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5),
+              ),
+              Text(
+                'bienvenue',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 18,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5),
+              ),
+              Text(
+                '$value',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 18,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5),
+              ),
             ],
           ),
         ));
