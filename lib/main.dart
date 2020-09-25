3af9aa5bb322930c2b252bbafe90d7bed3cbad13
JamelChatti <chatti261@gmail.com>
2020-09-25 23:18:39 +0200

    AlerteDialog

--- lib/main.dart
+++ lib/main.dart
@@ -22,17 +22,102 @@ class GestionFacture extends StatefulWidget {
   _GestionFactureState createState() => _GestionFactureState();
 }
 
+enum reponse {
+  OUI,
+  NON,
+  SAIS_PAS,
+}
+
 class _GestionFactureState extends State<GestionFacture> {
-  final GlobalKey<ScaffoldState>  _sb = GlobalKey<ScaffoldState>();
-  void _snackBar(){
-    _sb.currentState.showSnackBar(SnackBar(content: Text('Votre message a été supprimé')));
-        
+  List<BottomNavigationBarItem> _item;
+  int _id = 0;
+  String value = '';
+  Widget ic = Container();
+  String msg = '';
+
+  void _setvalue(String a) => setState(() => value = a);
+
+  void _setmsg(String b) => setState(() => msg = b);
+
+  void _seticon(Widget c) => setState(() => ic = c);
+
+  Future info(String a, Widget ic) async {
+    switch (await showDialog(
+      context: context,
+      child: SimpleDialog(
+        title: Text(
+          a.toString(),
+          style: TextStyle(color: Colors.black, fontSize: 18),
+        ),
+        children: <Widget>[
+          SimpleDialogOption(
+            child: Text('OUI'),
+            onPressed: () {
+              Navigator.pop(
+                context,
+                reponse.OUI,
+              );
+            },
+          ),
+          SimpleDialogOption(
+            child: Text('NON'),
+            onPressed: () {
+              Navigator.pop(
+                context,
+                reponse.NON,
+              );
+            },
+          ),
+          SimpleDialogOption(
+            child: Text('SAIS_PAS'),
+            onPressed: () {
+              Navigator.pop(
+                context,
+                reponse.SAIS_PAS,
+              );
+            },
+          )
+        ],
+      ),
+    )) {
+      case reponse.OUI:
+        _setvalue('OUI, je suis satisfait du service');
+        break;
+      case reponse.NON:
+        _setvalue('NON, je ne suis pas satisfait du service');
+        break;
+      case reponse.SAIS_PAS:
+        _setvalue('Je ne sais pas je suis partagé');
+        break;
+    }
+  }
+
+  void initState() {
+    super.initState();
+    _item = new List();
+    _item.add(BottomNavigationBarItem(
+        icon: Icon(
+          Icons.local_hospital,
+          color: Colors.purple,
+        ),
+        title: Text('Clinique')));
+    _item.add(BottomNavigationBarItem(
+        icon: Icon(
+          Icons.restaurant,
+          color: Colors.blue,
+        ),
+        title: Text('Restaurant')));
+    _item.add(BottomNavigationBarItem(
+        icon: Icon(
+          Icons.hotel,
+          color: Colors.green,
+        ),
+        title: Text('Hotel')));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _sb,
       appBar: AppBar(
         backgroundColor: Colors.cyan,
         title: Text('Tuto snackBar'),
@@ -48,16 +133,6 @@ class _GestionFactureState extends State<GestionFacture> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            RaisedButton(
-              onPressed: _snackBar,
-              child: Text(
-                'Supprimer message',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                ),
-              ),
-            ),
             Text(
               'bienvenue',
               style: TextStyle(
@@ -78,15 +153,51 @@ class _GestionFactureState extends State<GestionFacture> {
             Padding(
               padding: EdgeInsets.only(top: 5),
             ),
+
             Text(
-              'Bienvenue',
+              'Point de vu: ',
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 30,
+              ),
+            ),
+
+            Text(
+              '$value',
               style: TextStyle(
                 color: Colors.red,
                 fontSize: 30,
               ),
             ),
+
+            ic,
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _item,
+        fixedColor: Colors.blue,
+        currentIndex: _id,
+        onTap: (int item) {
+          _id = item;
+          setState(() {
+            if (_id == 0) ;
+            {
+              _setmsg('Etes vous satisfait du service fourni par la clinique');
+            }
+
+            if (_id == 1) ;
+            {
+              _setmsg('Etes vous satisfaitdu service par le restaurant');
+            }
+
+            if (_id == 2) ;
+            {
+              _setmsg('Etes vous satisfait du service par l\'hotel');
+            }
+            info(msg, ic);
+          });
+        },
       ),
     );
     //barre de navigation
