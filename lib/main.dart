b987d38fa5453cddc22259d81da16fd3f17e9d41
JamelChatti <chatti261@gmail.com>
2020-09-27 21:46:09 +0200

    ListView tuto N°24

--- lib/main.dart
+++ lib/main.dart
@@ -28,86 +28,505 @@ class _GestionFactureState extends State<GestionFacture> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text('Tuto snackBar'),
-        centerTitle: true,
-        actions: <Widget>[
-          Icon(
-            Icons.thumb_up,
-            size: 35,
-          ),
-        ],
-      ),
-      body:
-      Stack(
-        //alignment: Alignment.topRight,
-          children: <Widget>[
-      Flex(
-      direction: Axis.vertical,
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: Container(color: Colors.cyan,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Tuto snackBar'),
+          centerTitle: true,
+          actions: <Widget>[
+            Icon(
+              Icons.thumb_up,
+              size: 35,
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Tableau de bord Tunisair',
+                    style: TextStyle(color: Colors.blue[800], fontSize: 20, fontWeight: FontWeight.bold),
+                  )
+                ],
               ),
-            ),
-            Expanded(
-              flex: 3,
-                child: Container(color: Colors.black,
-                ))
-                ]
-      ),
-        Positioned(
-          top: 50,
-          left: 20,
-          right: 20,
-          child: Container(
-            height: 400,
-            width: 200,
-            color: Colors.amber,
-            child: Column(
-             // crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.all(10),
-                  height: 100,
-                  width: 100,
-                  color: Colors.lightGreenAccent,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.blue,
-                ),
-
-                Row(
-
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-
-                    Container(
-                      margin:EdgeInsets.all(5),
-                      height: 100,
-                      width: 100,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      margin:EdgeInsets.all(5),
-                      height: 100,
-                      width: 100,
-                      color: Colors.purple,
-                    )
-                  ],
-                )
-
-              ],
-            ),
+              Container(
+                height: 500,
+                width: 350,
+                color: Colors.blue[400],
+                child: viewer(),
+              )
+            ],
           ),
-        )
-
-          ]
-      ),
-    );
+        ));
     //barre de navigation
   }
+}
+
+Widget viewer() {
+  return ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(
+          Icons.flight_takeoff,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Tunis-Istambul',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Decollage 2:45',
+          style: TextStyle(
+              color: Colors.white, fontSize: 18, ),
+      ),
+        onTap: (){
+          debugPrint('Fly Tunis-stambul');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_land,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Istambul-Tunis',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Aterrissagee 12:35',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly stambul-Tunis');
+        },
+      ),
+
+      ListTile(
+        leading: Icon(
+          Icons.flight_takeoff,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Tunis-Istambul',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Decollage 2:45',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly Tunis-stambul');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_takeoff,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Istambul-Tunis',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Aterrissagee 12:35',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly stambul-Tunis');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_land,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Tunis-Istambul',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Decollage 2:45',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly Tunis-stambul');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_takeoff,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Istambul-Tunis',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Aterrissagee 12:35',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly stambul-Tunis');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_land,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Tunis-Istambul',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Decollage 2:45',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly Tunis-stambul');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_takeoff,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Istambul-Tunis',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Aterrissagee 12:35',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly stambul-Tunis');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_land,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Tunis-Istambul',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Decollage 2:45',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly Tunis-stambul');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_takeoff,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Istambul-Tunis',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Aterrissagee 12:35',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly stambul-Tunis');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_land,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Tunis-Istambul',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Decollage 2:45',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly Tunis-stambul');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_takeoff,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Istambul-Tunis',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Aterrissagee 12:35',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly stambul-Tunis');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_land,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Tunis-Istambul',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Decollage 2:45',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly Tunis-stambul');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_takeoff,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Istambul-Tunis',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Aterrissagee 12:35',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly stambul-Tunis');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_land,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Tunis-Istambul',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Decollage 2:45',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly Tunis-stambul');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_takeoff,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Istambul-Tunis',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Aterrissagee 12:35',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly stambul-Tunis');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_land,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Tunis-Istambul',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Decollage 2:45',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly Tunis-stambul');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_takeoff,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Istambul-Tunis',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Aterrissagee 12:35',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly stambul-Tunis');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_land,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Tunis-Istambul',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Decollage 2:45',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly Tunis-stambul');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_takeoff,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Istambul-Tunis',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Aterrissagee 12:35',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly stambul-Tunis');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_land,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Tunis-Istambul',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Decollage 2:45',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly Tunis-stambul');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_takeoff,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Istambul-Tunis',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Aterrissagee 12:35',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly stambul-Tunis');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_land,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Tunis-Istambul',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Decollage 2:45',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly Tunis-stambul');
+        },
+      ),
+      ListTile(
+        leading: Icon(
+          Icons.flight_takeoff,
+          color: Colors.yellow,
+          size: 40,
+        ),
+        title: Text(
+          'Istambul-Tunis',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text( 'Aterrissagee 12:35',
+          style: TextStyle(
+            color: Colors.white, fontSize: 18, ),
+        ),
+        onTap: (){
+          debugPrint('Fly Istambul-Tunis');
+        },
+      ),
+
+
+    ],
+  );
 }
