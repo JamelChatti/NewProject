3d6cb1aa04f03d4f3586a4f9934eb4ffa062827d
JamelChatti <chatti261@gmail.com>
2020-09-29 19:49:55 +0200

    TabBar Tuto N°26

--- lib/main.dart
+++ lib/main.dart
@@ -27,50 +27,73 @@ class GestionFacture extends StatefulWidget {
 class _GestionFactureState extends State<GestionFacture> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text('Tuto ListViewBuilder'),
-          centerTitle: true,
-          actions: <Widget>[
-            Icon(
-              Icons.thumb_up,
-              size: 35,
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: Text('Tuto ListViewBuilder'),
+            centerTitle: true,
+            actions: <Widget>[
+              Icon(
+                Icons.thumb_up,
+                size: 35,
+              ),
+            ],
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  child: Icon(Icons.alarm),
+                ),
+                Tab(
+                  child: Icon(Icons.present_to_all),
+                ),
+                Tab(
+                  child: Icon(Icons.directions_walk),
+                ),
+                Tab(
+                  child: Icon(Icons.airline_seat_individual_suite),
+                )
+              ],
             ),
-          ],
+          ),
+          body: TabBarView(
+            children: [
+              Card(
+                child: Image.asset(
+                  'images/th5.png',
+                  height: 200,
+                  width: 200,
+                ),
+              ),
+              Card(
+                child: Image.asset(
+                  'images/th6.png',
+                  height: 200,
+                  width: 200,
+                ),
+              ),
+              Card(
+                child: Image.asset(
+                  'images/th7.png',
+                  height: 800,
+                  width: 400,
+                ),
+              ),
+              Card(
+                child: Image.asset(
+                  'images/th5.png',
+                 // height: 100,
+                  //width: 100,
+                ),
+              ),
+            ],
+          ),
         ),
-        body:Container(
-          child: getListView(),
-        )
+      ),
     );
+
     //barre de navigation
   }
-}
-
-List<String> getListeElement() {
-  var item = List<String>.generate(25, (index) => 'Article:$index');
-  return item;
-}
-
-Widget getListView() {
-  var listitem = getListeElement();
-  var listview = ListView.builder(
-    itemCount: listitem.length ,
-      itemBuilder: (context, val) {
-    return ListTile(
-      title: Text(
-        listitem[val],
-        style: TextStyle(color: Colors.red, fontSize: 30),
-      ),
-      leading: Icon(
-        Icons.nature,
-        color: Colors.green,
-        size: 40,
-      ),
-      onTap: () {
-        debugPrint('Valeur active : $listitem[val]');
-      },
-    );
-  });
-  return listview;
 }
