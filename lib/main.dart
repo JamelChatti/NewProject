0ad41e620d85dcc558e309ece58b2da1e5850d73
JamelChatti <chatti261@gmail.com>
2020-10-08 14:05:52 +0200

    Backdrop filter Tuto N° 34

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -22,157 +25,70 @@ class GestionFacture extends StatefulWidget {
 }
 
 class _GestionFactureState extends State<GestionFacture> {
-  Widget resto(String menu, Color color, String image) {
-   return Container(
-      color: color,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Image.asset(
-            image,
-            height: 150,
-            width: 150,
-          ),
-          Text(
-            menu,
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
-          title: Text('Tuto Silver AppBar '),
+          title: Text('Tuto BackdropFilter '),
           centerTitle: true,
           actions: <Widget>[
             Icon(
-              Icons.local_cafe,
+              Icons.local_florist,
               size: 35,
             ),
           ],
         ),
-        body:  CustomScrollView(
-              slivers: <Widget>[
-                SliverAppBar(
-                  title: Text(
-                    'Tuto Silver AppBar',
-                    style: TextStyle(fontSize: 15, color: Colors.blue),
-                  ),
-                  centerTitle: true,
-                  pinned: true,
-                  floating: true,
-                  expandedHeight: 200,
-                  backgroundColor: Colors.transparent,
-                  flexibleSpace: FlexibleSpaceBar(
-                    background: Image.asset(
-                      'images/th6.png',
-                      fit: BoxFit.cover,
+        body: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: NetworkImage(
+                        'http://www.somuchviral.com/wp-content/uploads/2014/09/nature-photography27.jpg'),
+                  )),
+            ),
+            Positioned(
+                top: 100,
+                left: 50,
+                right: 50,
+                bottom: 400,
+                child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 0,
+                      sigmaY: 2,
                     ),
-                  ),
-                  actions: <Widget>[
-                    IconButton(
-                      icon: Icon(
-                        Icons.local_cafe,
-                        color: Colors.blue,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        color: Colors.greenAccent.withOpacity(0.5),
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              'Belle image',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 50,),
+                            Text(
+                              'Belle image',
+                              style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
                       ),
-                      onPressed: () {},
-                    )
-                  ],
-                ),
-                SliverFixedExtentList(
-                  itemExtent: 150,
-                  delegate: SliverChildListDelegate(
-                    [
-
-                     resto('3assida', Colors.green, 'images/3ss.png'),
-                            resto('Couscous', Colors.blue, 'images/th8.png'),
-                           resto('Mouloukhia', Colors.red, 'images/mou.png'),
-                            resto('Salade', Colors.yellow, 'images/sala.png'),
-                            resto('Mesfouf', Colors.green, 'images/mes.png'),
-                            resto('Riz jerbi', Colors.amber, 'images/riz.png'),
-                            resto('Fricasse', Colors.lightBlueAccent, 'images/fri.png'),
-                            resto('Michoui', Colors.purpleAccent, 'images/mich.png'),
-                            resto('Leblebi', Colors.lightGreen, 'images/lab.png'),
-                            resto('Mhamsa', Colors.grey, 'images/mha.png'),
-                            resto('Farfoucha', Colors.pinkAccent, 'images/th.png'),
-                  ],),
-                )
-              ],
-            )
-
-//         ListView(
-//           children:<Widget> [
-// imagecarousel,
-//           Form(
-//           key: formkey,
-//           child: Center(
-//             child: Column(
-//               children: <Widget>[
-//                 Text(
-//                   value,
-//                   style: TextStyle(fontSize: 20, color: Colors.blue),
-//                   textAlign: TextAlign.center,
-//                 ),
-//                 TextFormField(
-//                   decoration: InputDecoration(
-//                       labelText: 'Nom',
-//                       hintText: 'Entrer votre nom',
-//                       icon: Icon(
-//                         Icons.person,
-//                         color: Colors.deepPurple,
-//                         size: 50,
-//                       )),
-//                   validator:(val)=>val.length==0 ? 'Valider votre nom' : null ,
-//                   onSaved:(val)=> name=val ,
-//                   keyboardType: TextInputType.text,
-//                   autocorrect: true,
-//                   autofocus: true,
-//                   onChanged: affichage,
-//                   //onSubmitted: submit,
-//                 ),
-//                 TextFormField(
-//                   decoration: InputDecoration(
-//                       labelText: 'Phone',
-//                       hintText: 'Entrer votre numero de Téléphone',
-//                       icon: Icon(
-//                         Icons.phone,
-//                         color: Colors.deepPurple,
-//                         size: 50,
-//                       )),
-//                   validator:(val)=>val.length==0 ? 'Valider votre numero de telephone' : null ,
-//                   onSaved:(val)=> phone=val ,
-//                   keyboardType: TextInputType.number,
-//                 ),
-//                 TextFormField(
-//                   decoration: InputDecoration(
-//                       labelText: 'Mot de passe',
-//                       hintText: 'Entrer votre Mot de passe',
-//                       icon: Icon(
-//                         Icons.lock,
-//                         color: Colors.red,
-//                         size: 50,
-//                       )),
-//                   validator:(val)=>val.length==0 ? 'Valider le mot de passe' : null ,
-//                   onSaved:(val)=> mdp=val ,
-//                 ),
-//                 RaisedButton(
-//                   child:Text('Validation', style: TextStyle(color: Colors.blue, fontSize: 20, fontWeight: FontWeight.bold),) ,
-//                   onPressed: validationform,
-//                 )
-//               ],
-//             ),
-//           ),
-//         )
-//       ]//barre de navigation
-//         ),
-               );
+                    )))
+          ],
+        ));
   }
 }
