6e2e168fdd4e5d101d6d3691e7ce2faef5fb20be
JamelChatti <chatti261@gmail.com>
2020-10-08 12:00:10 +0200

    Sliver AppBar Tuto N°33

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:ui';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:carousel_pro/carousel_pro.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,130 +22,157 @@ class GestionFacture extends StatefulWidget {
 }
 
 class _GestionFactureState extends State<GestionFacture> {
-
-  Widget imagecarousel = Container(
-    height: 250,
-    child: Carousel(
-    boxFit: BoxFit.fill,
-    images: [
-      AssetImage('images/th9.png'),
-      AssetImage('images/th10.png'),
-      AssetImage('images/th11.png'),
-      AssetImage('images/th12.png'),
-      AssetImage('images/th13.png'),
-      AssetImage('images/th14.png'),
-      AssetImage('images/im15.png'),
-      AssetImage('images/im16.png'),
-    ],
-    ),
-  );
-
-  String value = ' ';
-  String name = ' ';
-  String phone = ' ';
-  String mdp = ' ';
-  final formkey=new GlobalKey<FormState>();
-
-  validationform(){
-   if (formkey.currentState.validate()){
-     formkey.currentState.save();
-     debugPrint('$name');
-     debugPrint('$phone');
-     debugPrint('$mdp');
-     formkey.currentState.reset();
-   }
-   else{
-     debugPrint('error....'); 
-   }
-  }
-
-
-  void affichage(String b) {
-    setState(() {
-      value = 'Bienvenue $b';
-    });
+  Widget resto(String menu, Color color, String image) {
+   return Container(
+      color: color,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Image.asset(
+            image,
+            height: 150,
+            width: 150,
+          ),
+          Text(
+            menu,
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: AppBar(
-        //   backgroundColor: Colors.blue,
-        //   title: Text('Tuto DataTable'),
-        //   centerTitle: true,
-        //   actions: <Widget>[
-        //     Icon(
-        //       Icons.desktop_mac,
-        //       size: 35,
-        //     ),
-        //   ],
-        // ),
-        body: ListView(
-          children:<Widget> [
-imagecarousel,
-          Form(
-          key: formkey,
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Text(
-                  value,
-                  style: TextStyle(fontSize: 20, color: Colors.blue),
-                  textAlign: TextAlign.center,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'Nom',
-                      hintText: 'Entrer votre nom',
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Tuto Silver AppBar '),
+          centerTitle: true,
+          actions: <Widget>[
+            Icon(
+              Icons.local_cafe,
+              size: 35,
+            ),
+          ],
+        ),
+        body:  CustomScrollView(
+              slivers: <Widget>[
+                SliverAppBar(
+                  title: Text(
+                    'Tuto Silver AppBar',
+                    style: TextStyle(fontSize: 15, color: Colors.blue),
+                  ),
+                  centerTitle: true,
+                  pinned: true,
+                  floating: true,
+                  expandedHeight: 200,
+                  backgroundColor: Colors.transparent,
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: Image.asset(
+                      'images/th6.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  actions: <Widget>[
+                    IconButton(
                       icon: Icon(
-                        Icons.person,
-                        color: Colors.deepPurple,
-                        size: 50,
-                      )),
-                  validator:(val)=>val.length==0 ? 'Valider votre nom' : null ,
-                  onSaved:(val)=> name=val ,
-                  keyboardType: TextInputType.text,
-                  autocorrect: true,
-                  autofocus: true,
-                  onChanged: affichage,
-                  //onSubmitted: submit,
+                        Icons.local_cafe,
+                        color: Colors.blue,
+                      ),
+                      onPressed: () {},
+                    )
+                  ],
                 ),
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'Phone',
-                      hintText: 'Entrer votre numero de Téléphone',
-                      icon: Icon(
-                        Icons.phone,
-                        color: Colors.deepPurple,
-                        size: 50,
-                      )),
-                  validator:(val)=>val.length==0 ? 'Valider votre numero de telephone' : null ,
-                  onSaved:(val)=> phone=val ,
-                  keyboardType: TextInputType.number,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'Mot de passe',
-                      hintText: 'Entrer votre Mot de passe',
-                      icon: Icon(
-                        Icons.lock,
-                        color: Colors.red,
-                        size: 50,
-                      )),
-                  validator:(val)=>val.length==0 ? 'Valider le mot de passe' : null ,
-                  onSaved:(val)=> mdp=val ,
-                ),
-                RaisedButton(
-                  child:Text('Validation', style: TextStyle(color: Colors.blue, fontSize: 20, fontWeight: FontWeight.bold),) ,
-                  onPressed: validationform,
+                SliverFixedExtentList(
+                  itemExtent: 150,
+                  delegate: SliverChildListDelegate(
+                    [
+
+                     resto('3assida', Colors.green, 'images/3ss.png'),
+                            resto('Couscous', Colors.blue, 'images/th8.png'),
+                           resto('Mouloukhia', Colors.red, 'images/mou.png'),
+                            resto('Salade', Colors.yellow, 'images/sala.png'),
+                            resto('Mesfouf', Colors.green, 'images/mes.png'),
+                            resto('Riz jerbi', Colors.amber, 'images/riz.png'),
+                            resto('Fricasse', Colors.lightBlueAccent, 'images/fri.png'),
+                            resto('Michoui', Colors.purpleAccent, 'images/mich.png'),
+                            resto('Leblebi', Colors.lightGreen, 'images/lab.png'),
+                            resto('Mhamsa', Colors.grey, 'images/mha.png'),
+                            resto('Farfoucha', Colors.pinkAccent, 'images/th.png'),
+                  ],),
                 )
               ],
-            ),
-          ),
-        )
-      ]//barre de navigation
-        ),
-    );
+            )
 
+//         ListView(
+//           children:<Widget> [
+// imagecarousel,
+//           Form(
+//           key: formkey,
+//           child: Center(
+//             child: Column(
+//               children: <Widget>[
+//                 Text(
+//                   value,
+//                   style: TextStyle(fontSize: 20, color: Colors.blue),
+//                   textAlign: TextAlign.center,
+//                 ),
+//                 TextFormField(
+//                   decoration: InputDecoration(
+//                       labelText: 'Nom',
+//                       hintText: 'Entrer votre nom',
+//                       icon: Icon(
+//                         Icons.person,
+//                         color: Colors.deepPurple,
+//                         size: 50,
+//                       )),
+//                   validator:(val)=>val.length==0 ? 'Valider votre nom' : null ,
+//                   onSaved:(val)=> name=val ,
+//                   keyboardType: TextInputType.text,
+//                   autocorrect: true,
+//                   autofocus: true,
+//                   onChanged: affichage,
+//                   //onSubmitted: submit,
+//                 ),
+//                 TextFormField(
+//                   decoration: InputDecoration(
+//                       labelText: 'Phone',
+//                       hintText: 'Entrer votre numero de Téléphone',
+//                       icon: Icon(
+//                         Icons.phone,
+//                         color: Colors.deepPurple,
+//                         size: 50,
+//                       )),
+//                   validator:(val)=>val.length==0 ? 'Valider votre numero de telephone' : null ,
+//                   onSaved:(val)=> phone=val ,
+//                   keyboardType: TextInputType.number,
+//                 ),
+//                 TextFormField(
+//                   decoration: InputDecoration(
+//                       labelText: 'Mot de passe',
+//                       hintText: 'Entrer votre Mot de passe',
+//                       icon: Icon(
+//                         Icons.lock,
+//                         color: Colors.red,
+//                         size: 50,
+//                       )),
+//                   validator:(val)=>val.length==0 ? 'Valider le mot de passe' : null ,
+//                   onSaved:(val)=> mdp=val ,
+//                 ),
+//                 RaisedButton(
+//                   child:Text('Validation', style: TextStyle(color: Colors.blue, fontSize: 20, fontWeight: FontWeight.bold),) ,
+//                   onPressed: validationform,
+//                 )
+//               ],
+//             ),
+//           ),
+//         )
+//       ]//barre de navigation
+//         ),
+               );
   }
 }
