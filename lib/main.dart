a65cc4ea81827e4e40e0063d2c2da906193f2b8d
JamelChatti <chatti261@gmail.com>
2020-10-16 18:07:20 +0200

    SingleChildScrollView tuto n°39

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,123 +23,159 @@ class GestionFacture extends StatefulWidget {
 }
 
 class _GestionFactureState extends State<GestionFacture> {
-
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text('Tuto StaggeredGridview '),
-          centerTitle: true,
-          actions: <Widget>[
-            Icon(
-              Icons.local_florist,
-              size: 35,
-            ),
-          ],
-        ),
-        body: Container(
-          margin: EdgeInsets.all(10),
-          child: StaggeredGridView.countBuilder(
-              crossAxisCount: 4,
-              mainAxisSpacing: 5.0,
-              crossAxisSpacing: 5.0,
-              shrinkWrap: true,
-              primary: false,
-              itemCount: infos.length,
-              itemBuilder: (BuildContext context, int index){
-                return Showinfo(
-                  verif: infos[index]
-                );
-              },
-              staggeredTileBuilder: (int index){
-                return StaggeredTile.fit(2);
-              }),
-        )
-    );
-  }
-}
-
-class Info {
-  String image;
-  String titles;
-  String city;
-
-  Info({this.image, this.titles, this.city});
-}
-
-List<Info> infos = [
-  Info(
-    image: 'https://tse2.mm.bing.net/th?id=OIP.EEFXJj_Zwm_NqzJeHIJE4QHaFU&pid=Api&P=0&w=213&h=154',
-    titles: 'Maison culture',
-    city: 'Msaken',
-  ),
-  Info(
-    image: 'https://tse1.mm.bing.net/th?id=OIP.cq8NMM3Yrqy_0SFD78B13AHaF7&pid=Api&P=0&w=190&h=153',
-    titles: 'Rond point mongala',
-    city: 'Msaken',
-  ),
-  Info(
-    image: 'https://tse3.mm.bing.net/th?id=OIP.6FD8jHV0MmHawVYXoBn5BQHaFq&pid=Api&P=0&w=199&h=154',
-    titles: 'Grande mosqué',
-    city: 'Msaken',
-  ),
-  Info(
-    image: 'https://tse3.mm.bing.net/th?id=OIP._8xBHE02zEkyMqPBosNfWwHaE_&pid=Api&P=0&w=226&h=153',
-    titles: 'Municipalité',
-    city: 'Msaken',
-  ),
-  Info(
-    image: 'https://tse4.mm.bing.net/th?id=OIP.3AVkjph3fSn8RMlMqUjY7QHaFj&pid=Api&P=0&w=216&h=163',
-    titles: 'Rue',
-    city: 'Sidibousaid',
-  ),
-  Info(
-    image: 'https://tse1.mm.bing.net/th?id=OIP.ShL2RLjLg8PZmrvnxYvtOgAAAA&pid=Api&P=0&w=235&h=177',
-    titles: 'vue de mer',
-    city: 'Sidibousaid',
-  ),
-  Info(
-    image: 'https://tse1.mm.bing.net/th?id=OIP.GJuV7AB_KybFpSnDxAes7AHaDF&pid=Api&P=0&w=393&h=165',
-    titles: 'oust dar ',
-    city: 'Tunis',
-  ),
-  Info(
-    image: 'https://tse1.mm.bing.net/th?id=OIP.jjytyoIm1g3SbyLXQJRGoAHaIV&pid=Api&P=0&w=300&h=300',
-    titles: 'Vue de l\'interieur maison ancienne',
-    city: 'Tunisie',
-  ),
-  Info(
-    image: 'https://tse3.mm.bing.net/th?id=OIP.ltPRx3cOS-spMeIccipO5QHaLF&pid=Api&P=0&w=300&h=300',
-    titles: 'Vue de l\'interieur maison ancienne',
-    city: 'Tunisie',
-  ),
-  Info(
-    image: 'https://tse1.mm.bing.net/th?id=OIP.RHRccPCKqZfchoac7fh5pQAAAA&pid=Api&P=0&w=300&h=300',
-    titles: 'Vue de l\'interieur maison ancienne',
-    city: 'Tunisie',
-  ),
-];
-
-class Showinfo extends StatelessWidget {
-  final Info verif;
-
-  const Showinfo({Key key, this.verif}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: Image.network(verif.image),
-
-        ),
-        Text(verif.titles, style: TextStyle(fontWeight: FontWeight.bold),),
-        Text(verif.city, style: TextStyle(fontWeight: FontWeight.bold),),
-        SizedBox(height: 30)
-      ],
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: DefaultTabController(
+            length: 2,
+            child: Scaffold(
+                appBar: AppBar(
+                  backgroundColor: Colors.blue,
+                  title: Text('Tuto SingleChildScrollView '),
+                  centerTitle: true,
+                  actions: <Widget>[
+                    Icon(
+                      Icons.local_florist,
+                      size: 35,
+                    ),
+                  ],
+                  bottom: TabBar(tabs: <Widget>[
+                    Tab(
+                      child: Icon(
+                        Icons.home,
+                        color: Color(0xff622F74),
+                        size: 40,
+                      ),
+                    ),
+                    Tab(
+                      child: Icon(
+                        Icons.pets,
+                        color: Color(0xff622F74),
+                        size: 40,
+                      ),
+                    ),
+                  ]),
+                ),
+                body: TabBarView(
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                        children: <Widget>[
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Container(
+                              margin: EdgeInsets.only(top: 20),
+                              child: Row(
+                                children: <Widget>[
+                                  ClipRRect(
+                                      borderRadius: BorderRadius.circular(20),
+                                      child: Image.network(
+                                        'https://tse3.mm.bing.net/th?id=OIP.5vtUZIF068A7Ox4nwwlB4wHaLH&pid=Api&P=0&w=300&h=300',
+                                        height: 300,
+                                        width: 300,
+                                      )),
+                                  SizedBox(
+                                    width: 50,
+                                  ),
+                                  ClipRRect(
+                                      borderRadius: BorderRadius.circular(20),
+                                      child: Image.network(
+                                        'https://tse1.explicit.bing.net/th?id=OIP.DxhjYW77E3v2k71GRPAa6AHaEK&pid=Api&P=0&w=286&h=161',
+                                        height: 300,
+                                        width: 300,
+                                      )),
+                                  SizedBox(
+                                    width: 50,
+                                  ),
+                                  ClipRRect(
+                                      borderRadius: BorderRadius.circular(20),
+                                      child: Image.network(
+                                        'https://tse4.mm.bing.net/th?id=OIP.8m7D9rvUrmLrmSqj-l7VSgHaFj&pid=Api&P=0&w=214&h=161',
+                                        height: 300,
+                                        width: 300,
+                                      )),
+                                  SizedBox(
+                                    width: 50,
+                                  ),
+                                  ClipRRect(
+                                      borderRadius: BorderRadius.circular(20),
+                                      child: Image.network(
+                                        'https://tse3.mm.bing.net/th?id=OIP.yAbvtnVcerpb6LfpxWdytQHaHb&pid=Api&P=0&w=300&h=300',
+                                        height: 300,
+                                        width: 300,
+                                      )),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: Container(
+                        child: Column(
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(100),
+                              child: Image.network(
+                                'https://tse4.mm.bing.net/th?id=OIP.G-hDSfttOTRj2_2Z5pSMYQHaEK&pid=Api&P=0&w=286&h=161',
+                                height: 400,
+                                width: 300,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 0,
+                            ),
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(100),
+                              child: Image.network(
+                                'https://tse1.mm.bing.net/th?id=OIP.l0aTUHXKLov2Ionv65hYcwHaFs&pid=Api&P=0&w=219&h=170',
+                                height: 400,
+                                width: 300,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(100),
+                              child: Image.network(
+                                'https://tse1.mm.bing.net/th?id=OIP.8M1SoX2xdufbRSqeXbwMxgHaEK&pid=Api&P=0&w=314&h=177',
+                                height: 400,
+                                width: 300,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(100),
+                              child: Image.network(
+                                'https://tse3.mm.bing.net/th?id=OIP.FeBKmr3l9kSZI97UphVMBQHaFj&pid=Api&P=0&w=225&h=169',
+                                height: 400,
+                                width: 300,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(100),
+                              child: Image.network(
+                                'https://tse1.mm.bing.net/th?id=OIP.rbgoISCUJX6l5bX6ppJ6-gHaEo&pid=Api&P=0&w=293&h=184',
+                                height: 400,
+                                width: 300,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ))));
   }
 }
